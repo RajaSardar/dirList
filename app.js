0f2f9ce87d63ac6cbd27ee299a0a897c41de6fd1
Raja Sardar <rj.raj@outlook.com>
2021-04-22 14:53:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -29,4 +29,24 @@ const dirList = (directory_String, add_Extention='') =>{
     // console.log(listDir);
 }
 
+const dirListByString = (dir_String_multiline_or_single,add_Extention_Without_Dot="") => {
+    if(!dir_String_multiline_or_single) {
+        console.log("Error: pass a valid string");
+        return;
+    };
+    dir_String_multiline_or_single = dir_String_multiline_or_single.toString().replace(/\s(?=\s)/g,'').replace(/\n/g,' ').trim();
+    console.log("Input letter Length :",dir_String_multiline_or_single.length);
+    console.log("Input Word List :",dir_String_multiline_or_single);
+    const cd = dir_String_multiline_or_single.split(" ");
+    if(add_Extention_Without_Dot) {
+        const ef = cd.map(e=> e+'.'+add_Extention_Without_Dot);
+        console.log("Output Word length :",ef.length);
+        console.log("Output :",ef.join(" "));
+    }
+    else{
+        console.log("Input length :",cd.length);
+    }
+}
+
+module.exports = dirListByString;
 module.exports = dirList;
